eab9381828838220c867f58e0fcdc3c677816be7
David <dalau6@gmail.com>
2020-05-04 14:33:48 -0700

    Undetermined Geolocation

--- components/Live.js
+++ components/Live.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 export default function Live() {
   const [coords, useCoords] = React.useState(null);
   const [status, useStatus] = React.useState(null);
   const [direction, useDirection] = React.useState('');
+
+  const askPermission = () => {};
 
   if (status === null) {
     return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -20,8 +30,12 @@ export default function Live() {
 
   if (status === 'undetermined') {
     return (
-      <View>
-        <Text>undetermined</Text>
+      <View style={styles.center}>
+        <Foundation name="alert" size={50} />
+        <Text>You need to enable location services for this app.</Text>
+        <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+          <Text style={styles.buttonText}>Enable</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -33,3 +47,28 @@ export default function Live() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
