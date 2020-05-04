577fe2edbd883dffa02457937ba7ae1114831b8c
David <dalau6@gmail.com>
2020-05-03 23:13:10 -0700

    navigationOptions

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -8,3 +8,15 @@ export default function EntryDetail({ navigation }) {
     </View>
   );
 }
+
+EntryDetail.prototype.navigationOptions = ({ navigation }) => {
+  const { entryId } = navigation.state.params;
+
+  const year = entryId.slice(0, 4);
+  const month = entryId.slice(5, 7);
+  const day = entryId.slice(8);
+
+  return {
+    title: `${month}/${day}/${year}`,
+  };
+};
